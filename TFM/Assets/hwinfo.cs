8ed30a247e0e902cc4bdb2fb36c3edddfab0c010
Tylemagne <tyler11440_rs2@hotmail.com>
2017-12-22 21:52:36 -0600

    UI dummy completed

---
+++ TFM/Assets/hwinfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hwinfo : MonoBehaviour {
+
+	public Text gpu2;
+
+	// Use this for initialization
+	void Start () {
+
+		//Debug.Log (SystemInfo.graphicsDeviceType);
+		Debug.Log ("NAME "+SystemInfo.graphicsDeviceName);
+		gpu2.text = SystemInfo.graphicsDeviceName;
+		Debug.Log ("DID "+ SystemInfo.graphicsDeviceID);
+		Debug.Log (SystemInfo.graphicsDeviceType);
+		Debug.Log (SystemInfo.graphicsDeviceVendor);
+		Debug.Log (SystemInfo.graphicsDeviceVendorID);
+		Debug.Log (SystemInfo.graphicsDeviceVersion);
+		Debug.Log (SystemInfo.graphicsMemorySize);
+		Debug.Log (SystemInfo.graphicsMultiThreaded);
+		Debug.Log (SystemInfo.graphicsShaderLevel);
+
+		Debug.Log (SystemInfo.deviceModel);
+		Debug.Log (SystemInfo.deviceName);
+		Debug.Log (SystemInfo.deviceType);
+		Debug.Log (SystemInfo.deviceUniqueIdentifier);
+
+		Debug.Log (SystemInfo.processorCount);
+		Debug.Log (SystemInfo.processorFrequency);
+		Debug.Log (SystemInfo.processorType);
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
